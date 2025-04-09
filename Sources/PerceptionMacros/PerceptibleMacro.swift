28828e3c08ac8e71366def08dec66dd27a79c89b
Jacob Bartlett <jacob@lapse.com>
2025-04-09 15:41:13 +0100

    fix: Swift.type

--- Sources/PerceptionMacros/PerceptibleMacro.swift
+++ Sources/PerceptionMacros/PerceptibleMacro.swift
@@ -64,7 +64,7 @@ public struct PerceptibleMacro {
       keyPath: KeyPath<\(perceptibleType), Member>,
       _ mutation: () throws -> MutationResult
       ) rethrows -> MutationResult {
-      let typeName = String(describing: type(of: self))
+      let typeName = String(describing: Swift.type(of: self))
       let keyPathDescription = String(describing: keyPath)
       print("Perceptible withMutation: \\(typeName).\\(keyPathDescription)")
       return try \(raw: registrarVariableName).withMutation(of: self, keyPath: keyPath, mutation)
